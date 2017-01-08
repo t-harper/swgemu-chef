8a7ffb8450367c6970bfb001b5dbb8bda59304cc
Travis Harper <tharper@ghost.attlocal.net>
2017-01-08 00:21:26 -0500

    Initial Commit

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "https://github.com/holms/vagrant-centos7-box/releases/download/7.1.1503.001/CentOS-7.1.1503-x86_64-netboot.box"
+
+  config.vm.provision "chef_apply" do |chef|
+    chef.recipe = File.read("swgemu_base.rb")
+  end
+
+  config.vm.provider 'virtualbox' do |v|
+    v.memory = 4096
+    v.cpus = 2
+  end
+
+  for i in 44452..44463
+    config.vm.network :forwaded_port, guest: i, host:i
+  end
+
+  config.vm.network "forwarded_port", guest: 3306, host: 3307
+  config.vm.network "forwarded_port", guest: 44452, host: 44452
+end
---
+++ swgemu_base.rb
@@ -0,0 +1,125 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+remote_file "/tmp/mysql-community-release-el7-5.noarch.rpm" do
+  source "http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm"
+end
+
+package 'mysql_community_repo' do
+  source "/tmp/mysql-community-release-el7-5.noarch.rpm"
+end
+
+%w(
+  gcc-c++
+  make
+  gdb
+  automake
+  git
+  readline-devel
+  openssl-devel
+  mysql-community-server
+  mysql-community-devel
+  java-1.8.0-openjdk-devel
+  vim-enhanced
+  git
+).each do |p|
+  package p do
+    action :install
+  end
+end
+
+service "mysqld" do
+  action [ :enable, :start ]
+end
+
+bash "install_default_database" do
+  code <<-EOF
+  mysql -u root -e "GRANT ALL PRIVILEGES ON swgemu.* TO 'swgemu'@'127.0.0.1' IDENTIFIED BY 'development'"
+  mysql -u root < /vagrant/MMOCoreORB/sql/swgemu.sql;
+  mysql -u root swgemu < /vagrant/MMOCoreORB/sql/datatables.sql;
+  mysql -u root swgemu < /vagrant/MMOCoreORB/sql/mantis.sql;
+  EOF
+  not_if "mysql -u root -e 'show databases' | grep swgemu"
+end
+
+link "/usr/lib64/libmysqlclient.so" do
+  to "/usr/lib64/mysql/libmysqlclient.so"
+end
+
+remote_file "/tmp/db-5.0.32.NC.tar.gz" do
+  source "http://download.oracle.com/berkeley-db/db-5.0.32.NC.tar.gz"
+end
+
+#bash 'install_berkeley' do
+#  cwd '/tmp'
+#  code <<-EOF
+#  tar -xf db-5.0.32.NC.tar.gz
+#  cd db-5.0.32.NC/build_unix
+#  ../dist/configure
+#  make
+#  make install
+#  EOF
+#  not_if { ::File.exist?("/usr/local/BerkeleyDB.5.0/lib/libdb.so") }
+#end
+
+#template "/etc/ld.so.conf.d/berkeley-x86_64.conf" do
+#  source "/vagrant/templates/berkeley-x86_64.conf.erb"
+#  local true
+#  owner "root"
+#  group "root"
+#  mode "0644"
+#end
+
+#template "/etc/profile.d/berkeley.sh" do
+#  source "/vagrant/templates/berkeley.sh.erb"
+#  local true
+#  owner "root"
+#  group "root"
+#  mode "0644"
+#end
+
+package "libdb"
+package "libdb-devel"
+package "libdb-cxx"
+package "libdb-cxx-devel"
+
+remote_file "/tmp/lua-5.3.3.tar.gz" do
+  source "https://www.lua.org/ftp/lua-5.3.3.tar.gz"
+end
+
+bash 'install_lua' do
+  cwd '/tmp'
+  code <<-EOF
+  tar -xf lua-5.3.3.tar.gz
+  cd lua-5.3.3
+  make clean
+  make linux
+  make install
+  EOF
+  not_if { ::File.exist?("/usr/local/bin/luac") }
+end
+
+link "/usr/local/bin/idlc" do
+  to "/vagrant/MMOEngine/bin/idlc"
+end
+
+template "/etc/profile.d/swgemu.sh" do
+  source "/vagrant/templates/swgemu.sh.erb"
+  local true
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+
+file "/vagrant/MMOEngine/bin/idlc" do
+  mode "0755"
+end
+
+template "/vagrant/MMOCoreORB/bin/conf/config.lua" do
+  source "/vagrant/templates/config.lua.erb"
+  local true
+  owner "vagrant"
+  group "vagrant"
+  mode "0644"
+end
